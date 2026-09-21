e5d56b2f91dbf7ec759d4353dbb36810c9e1390f
Yuriafer <yuriafer@gmail.com>
2026-09-20 21:52:43 -0300

    Add files via upload
    diminuição do numero de processos para 2 em -paralelo-

--- paralelo.py
+++ paralelo.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     inicio = 1
     fim = 50_000_000
 
-    quantidade_processos = 4
+    quantidade_processos = 2
 
     print(f"Procurando números primos de {inicio} até {fim}...")
     print(f"Processos utilizados: {quantidade_processos}")
